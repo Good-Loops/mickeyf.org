6ca3fb7013ed5a52033040a39ca3004c7f8b3c61
Michel Dias <50039888+Good-Loops@users.noreply.github.com>
2023-05-15 13:25:48 -0300

    Comment out test

--- frontend/components/DanceCircles.ts
+++ frontend/components/DanceCircles.ts
@@ -2,10 +2,10 @@ import danceCircles from "../public/src/danceCircles";
 function component() {
     const render = function() {
         return `
-            <h1 id="dc-title">Dancing Circles</h1>
+            <!-- <h1 id="dc-title">Dancing Circles</h1>
             <canvas id="dancing-circles"></canvas>
             <label id="upload-button" for="file-upload">Upload Music</label>
-            <input type="file" name="file" id="file-upload" multiple />
+            <input type="file" name="file" id="file-upload" multiple /> -->
             <input type="file" name="file" multiple />
         `;
     }
