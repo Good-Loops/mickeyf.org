717a7675065d5e978561f50def478391a7a64128
Michel Dias <michel.sdf@gmail.com>
2024-03-07 21:16:17 -0300

    Trying to implement svg icons

--- pages/SocialMedia.ts
+++ pages/SocialMedia.ts
@@ -5,13 +5,21 @@ function component() {
                 <h1 class="social-media__title">Social Media</h1>
                 <div class="social-media__grid">
                     <a href="https://www.tiktok.com/@mickeyf.plays" target="_blank">
-                        <h3 class="social-media__grid-item--tiktok floating">TikTok</h3>
+                        <svg class="social-media__grid-item--tiktok floating">
+                            <use xlink:href="../../assets/img/sprite.svg#tiktok"></use>
+                        </svg>
                     </a>
                     <a href="https://www.instagram.com/mickeyf.plays/" target="_blank">
-                        <h3 class="social-media__grid-item--instagram floating">Instagram</h3>
+                        <!-- <h3 class="social-media__grid-item--instagram floating">Instagram</h3> -->
+                        <svg class="social-media__grid-item--instagram floating">
+                            <use xlink:href="../../assets/img/sprite.svg#instagram"></use>
+                        </svg>
                     </a>
                     <a href="https://www.youtube.com/@mickeyfplays" target="_blank">
-                        <h3 class="social-media__grid-item--youtube floating">Youtube</h3>
+                        <!-- <h3 class="social-media__grid-item--youtube floating">Youtube</h3> -->
+                        <svg class="social-media__grid-item--youtube floating">
+                            <use xlink:href="../../assets/img/sprite.svg#youtube"></use>
+                        </svg>
                     </a>
                 </div>
             </section>
