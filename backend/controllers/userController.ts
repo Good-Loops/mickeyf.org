e4c7af2c6922db5b1dadc30b4cc28292f171875e
Michel Dias <michel.sdf@gmail.com>
2024-04-07 13:18:57 -0300

    Updated userController and customTypes

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -2,15 +2,17 @@ import { Request, Response } from 'express';
 import { RowDataPacket } from 'mysql2';
 import { pool } from '../config/dbConfig';
 import { IUser } from '../types/customTypes';
+import bcrypt from 'bcrypt';
 
 export const getUsers = async (req: Request, res: Response) => {
     try {
+        // Get all users from database
         const [rows] = await pool.query<RowDataPacket[]>('SELECT * FROM users');
         const users: IUser[] = rows as any[];
         res.json(users);
     } catch (error) {
         if (error instanceof Error) {
-            // Now we can safely access error.message
+            // Safely access error.message
             res.status(500).send(error.message);
         } else {
             // We can't be sure what type error is, so it's best to not touch it
@@ -20,19 +22,23 @@ export const getUsers = async (req: Request, res: Response) => {
 }
 
 export const addUser = async (req: Request, res: Response) => {
-    const { username, email } = req.body as IUser;
+    // Destructure the request body
+    const { username, email, password } = req.body as IUser;
+    // Hash password
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
     try {
+        // Insert user into database
         const [result] = await pool.query(
-            'INSERT INTO users (username, email) VALUES (?, ?)',
-            [username, email]
+            'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+            [username, email, hashedPassword]
         );
-        res.json(result);
+        res.json({ success: true, message: "User added successfully!" });
     } catch (error) {
         if (error instanceof Error) {
-            // Now we can safely access error.message
             res.status(500).send(error.message);
         } else {
-            // We can't be sure what type error is, so it's best to not touch it
             res.status(500).send('An unexpected error occurred');
         }
     }
--- backend/types/customTypes.ts
+++ backend/types/customTypes.ts
@@ -2,4 +2,5 @@ export interface IUser {
     id: number;
     username: string;
     email: string;
+    password: string;
 }
