5aae23ed7ee1a33d558b4df6aeb4d9984d1e450d
Michel Dias <michel.sdf@gmail.com>
2024-04-13 10:24:33 -0300

    Working on server structure

--- backend/config/dbConfig.ts
+++ backend/config/dbConfig.ts
@@ -1,5 +1,7 @@
 import { createPool } from 'mysql2/promise';
 
+console.log(process.env.DB_HOST);
+
 export const pool = createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -9,3 +11,7 @@ export const pool = createPool({
     connectionLimit: 10,
     queueLimit: 0
 });
+
+pool.getConnection()
+    .then(conn => console.log("Database connected successfully!"))
+    .catch(err => console.error("Database connection failed:", err));
