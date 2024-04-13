c9173561981c0c7ccf17b01a196b5728ab96f63c
Michel Dias <michel.sdf@gmail.com>
2024-04-13 13:23:36 -0300

    Updated User.ts to integrate Page.js

--- frontend/components/User.ts
+++ frontend/components/User.ts
@@ -1,39 +1,33 @@
-import hashInfo from "../src/utils/hashInfo";
 import { getUserData } from "../src/utils/methods";
 
 function component() {
-
-    const user = async function() {
+    const user = async function(id: string) {
         
-        const { queryParameters } = hashInfo();
-        const id = queryParameters.get('user_id');
+        const data = await getUserData();
 
-        const data = await getUserData().then(data => {
-            return data;
-        });
+        const userData = data.find((user: { user_id: string; }) => user.user_id === id);
 
-        const user = data.find((user: { user_id: string; }) => user.user_id === id) ?? "";
-        
-        return user;
+        if (!userData) {
+            throw new Error('User not found');
+        }
+
+        return userData;
     }
 
-    const render = async function() {
-        const userData: {
-            user_id: string;
-            user_name: string;
-            email: string;
-            user_password: string;
-        } = await user();
-        
-        if (!userData) {
-            window.location.hash = "#/error";
+    const render = async function (params: { id: string }) {
+        try {
+            const userData = await user(params.id);
+
+            return /*html*/`
+                <div>
+                    Hello ${userData.user_name}
+                </div>
+            `;
+        } catch (error) {
+            // Redirect or handle errors
+            console.error(error);
+            return /*html*/`<div>User not found</div>`;
         }
-        
-        return /*html*/`
-            <div>
-                Hello ${userData.user_name}
-            </div>
-        `;
     }
 
     return {
