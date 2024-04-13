b9ab63b62455b48a78e3588290a66a2854b7af28
Michel Dias <michel.sdf@gmail.com>
2024-04-13 14:44:40 -0300

    Adjusted webpack again

--- frontend/components/Home.ts
+++ frontend/components/Home.ts
@@ -1,3 +1,5 @@
+import page from 'page';
+
 function component() {
     const render = function() {
         return /*html*/`
@@ -5,12 +7,19 @@ function component() {
                 <ul>
                     <template x-for="(users, index) in data" :key="index">
                         <li>
-                            <span x-text="users.user_name"></span> <a x-bind:href="'/#/user/' + users.user_id">Click here</a>
+                            <span x-text="users.user_name"></span>
+                            <a x-bind:href="'/user/' + users.user_id" x-on:click="navigateToUser">Click here</a>
                         </li>
                     </template>
                 </ul>
             </div>
         `;
+    }
+
+    const navigateToUser = (event: { target: { getAttribute: (arg0: string) => any; }; preventDefault: () => void; }) => {
+        const userId = event.target.getAttribute('href');
+        page.show(userId); // Use page.js for navigation
+        event.preventDefault(); // Prevent default link behavior
     }
 
     return {
