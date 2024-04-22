1e486f340dd8263f61ab265200d740fbfac137c3
Michel Dias <michel.sdf@gmail.com>
2024-04-21 21:40:31 -0300

    Continuing responsiveness for header/sidebar

--- frontend/src/events/EventListenerManager.ts
+++ frontend/src/events/EventListenerManager.ts
@@ -8,12 +8,15 @@ class EventListenerManager {
         const sideBarButton = document.querySelector('.sidebar-btn') as HTMLElement;
         const closeButton = document.querySelector('.close-btn') as HTMLElement;
 
-        if (sidebar && sideBarButton) { // Check if sidebar and sidebar button exist
-            sideBarButton.addEventListener('click', () => {
+        if (sidebar && sideBarButton && closeButton) { // Check if sidebar, sidebar button, and close button exist
+            const toggleVisibility = () => {
                 sidebar.classList.toggle('is-visible'); // Toggle sidebar visibility
                 sideBarButton.classList.toggle('is-visible'); // Toggle sidebar button visibility
                 closeButton.classList.toggle('is-visible'); // Toggle close button visibility
-            });
+            };
+
+            sideBarButton.addEventListener('click', toggleVisibility);
+            closeButton.addEventListener('click', toggleVisibility);
         }
     }
 }
