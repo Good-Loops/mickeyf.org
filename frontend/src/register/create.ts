fe5bb6471752df6f1e2804fc93f9f82d61aad26f
Michel Dias <michel.sdf@gmail.com>
2024-04-24 22:30:43 -0300

    Fixed api route

--- frontend/src/register/create.ts
+++ frontend/src/register/create.ts
@@ -14,7 +14,7 @@ function create(): IUserCreate {
             const email = (<HTMLInputElement>document.getElementById('email')).value;
             const user_password = (<HTMLInputElement>document.getElementById('password')).value;
 
-            fetch('http://localhost:3000/api/users', {
+            fetch('https://mickeyf-org-j7yuum4tiq-uc.a.run.app/api/users', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
