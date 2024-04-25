66aedb6d4cd236a6f0db44f1696d3196a9f4b97b
Michel Dias <michel.sdf@gmail.com>
2024-04-25 19:54:04 -0300

    Updated dbConfig.ts

--- backend/config/dbConfig.ts
+++ backend/config/dbConfig.ts
@@ -1,26 +1,26 @@
 import { createPool } from 'mysql2/promise';
 
 // For production
-// export const pool = createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     database: process.env.DB_NAME,
-//     password: process.env.DB_PASS,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-// });
-
-// For development
 export const pool = createPool({
-    host: 'localhost',
-    user: 'cms_mickeyf',
-    password: '*<HLecm?,el(S>@H',
-    database: 'cms',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
+
+// For development
+// export const pool = createPool({
+//     host: 'localhost',
+//     user: 'cms_mickeyf',
+//     password: '*<HLecm?,el(S>@H',
+//     database: 'cms',
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// });
 
 pool.getConnection()
     .then(conn => console.log("Database connected successfully!"))
