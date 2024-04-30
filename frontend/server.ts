80e0088695bfbab723b97c9290a66e17950f2795
Michel Dias <michel.sdf@gmail.com>
2024-04-30 07:48:45 -0300

    Trying to fix reload problem

--- frontend/server.ts
+++ frontend/server.ts
@@ -18,16 +18,20 @@ app.use('/api', createProxyMiddleware({
 }));
 
 // Serve static files from the root of the project
-app.use(serveStatic(publicPath, { 'index': ['index.html', 'index.htm'] }));
+app.use(serveStatic(publicPath, { index: ['index.html', 'index.htm'] }));
+
+// This route will handle all other GET requests to give control to the SPA
+app.get('*', (req, res) => {
+    console.log('Request path:', req.path);
+    if (req.method === 'GET' && !req.originalUrl.includes('/api')) {
+        console.log('Serving index.html for path:', req.path);
+        res.sendFile('index.html', { root: publicPath });
+    }
+});
 
 // 404 handling
 app.use(function (_req: any, res: { status: (arg0: number) => { (): any; new(): any; sendFile: { (arg0: string, arg1: { root: any; }): void; new(): any; }; }; }, _next: any) {
     res.status(404).sendFile('404.html', { root: publicPath });
-});
-
-// This route will handle all other get requests to give control to the SPA
-app.get('*', function (_req: any, res: { sendFile: (arg0: string, arg1: { root: any; }) => void; }) {
-    res.sendFile('index.html', { root: publicPath });
 });
 
 const port = process.env.PORT || 7777;
