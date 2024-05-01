6330a9d8ae5219ceacc176ad2a334407085fe08e
Michel Dias <michel.sdf@gmail.com>
2024-05-01 11:17:37 -0300

    Adjusted project for local development

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,14 @@
-require('dotenv').config();
-
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import userRoutes from './routes/userRoutes';
 import path from 'path';
+import dotenv from 'dotenv';
 
+// Load environment variables from the .env file
+dotenv.config();
+
+// Create an Express application
 const app = express();
 
 // Serve static files from assets
@@ -15,23 +18,49 @@ app.use(express.static('../frontend/public'));
 app.use(
     helmet.contentSecurityPolicy({
         directives: {
-            defaultSrc: ["'none'"], // Sets the default source to 'none'
-            imgSrc: ["https://mickeyf-org-j7yuum4tiq-uc.a.run.app"], // Allows images from these backend domains
-            scriptSrc: ["'self'"], // Allows scripts from the same origin
+            defaultSrc: ["'self'"],
+            connectSrc: [
+                "'self'",
+                ...(process.env.NODE_ENV === 'development' ? ["http://localhost:8080"] : ["https://mickeyf-org-j7yuum4tiq-uc.a.run.app"])
+            ],
+            imgSrc: [
+                "'self'",
+                "https://mickeyf-org-j7yuum4tiq-uc.a.run.app",
+                "http://localhost:8080",
+                "data:"
+            ],
+            scriptSrc: [
+                "'self'",
+                "'unsafe-inline'", // Remove in production
+                ...(process.env.NODE_ENV === 'development' ? ["http://localhost:8080"] : [])
+            ],
+            styleSrc: [
+                "'self'",
+                "'unsafe-inline'", // Remove in production
+                "https://fonts.googleapis.com",
+                ...(process.env.NODE_ENV === 'development' ? ["http://localhost:8080"] : [])
+            ],
+            fontSrc: [
+                "'self'",
+                "https://fonts.gstatic.com",
+                ...(process.env.NODE_ENV === 'development' ? ["http://localhost:8080"] : [])
+            ],
         }
     })
 );
 
+
+// Parse JSON bodies
 app.use(express.json());
 
 // CORS configuration
-app.use(cors(
-    {
-        origin: ['https://mickeyf-org-j7yuum4tiq-uc.a.run.app', 'https://mickeyf.org'], // Allow requests from these domains
-        methods: ['GET', 'POST'], // Allowed HTTP methods
-        allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
-    }
-));
+app.use(cors({
+    origin: process.env.NODE_ENV === 'development' ?
+        ['http://localhost:8080'] :
+        ['https://mickeyf-org-j7yuum4tiq-uc.a.run.app', 'https://mickeyf.org'],
+    methods: ['GET', 'POST'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
+}));
 
 // Use userRoutes for all user-related endpoints
 app.use('/api', userRoutes);
