f3f829f6fbfa39e19102c43b85f66912eac753cf
Michel Dias <michel.sdf@gmail.com>
2024-05-08 16:09:37 -0300

    Docker-compose working correctly

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
+        fallback: {
+            "path": require.resolve("path-browserify"),
+            "os": require.resolve("os-browserify/browser"),
+            "crypto": require.resolve("crypto-browserify"),
+            "vm": require.resolve("vm-browserify"),
+            "buffer": require.resolve("buffer/"),
+            "stream": require.resolve("stream-browserify")
+        }
     },
     output: {
         filename: '[name].min.js',
