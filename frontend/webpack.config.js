d3f2ed8cd7b1dd61f7ff0628b9035649c7e44c31
Michel Dias <michel.sdf@gmail.com>
2024-05-08 12:57:01 -0300

    Implementing dev env

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
     ],
     devServer: {
         static: {
-            directory: path.join(__dirname, 'dist'),
+            directory: path.join(__dirname, 'public'),
         },
         compress: true,
         port: 3000,
