b8ac398a45f18b5d5f3f0be11d9c789da1c1e8f5
Michel Dias <michel.sdf@gmail.com>
2024-05-15 17:45:04 -0300

    userRoutes

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,21 +1,8 @@
 import { Router } from 'express';
-import { addUser, loginUser } from '../controllers/userController';
+// import { handleUser } from '../controllers/userController';
 
 const router = Router();
 
-// router.get('/users', getUsers);
-
-router.post('/users', (req, res) => {
-    switch (req.body.type) {
-        case 'signup':
-            addUser(req, res);
-            break;
-        case 'login':
-            loginUser(req, res);
-            break;
-        default:
-            res.status(400).send({ error: 'Invalid type' });
-    }
-});
+// router.post('/users', handleUser);
 
 export default router;
