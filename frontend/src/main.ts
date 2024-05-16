c7ca3b68acfb4e325b65c1eb68211c1a18e5bf3e
Michel Dias <michel.sdf@gmail.com>
2024-05-16 12:38:59 -0300

    Updated firebaseConfig to use .env variables

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,31 +1,33 @@
 require('dotenv').config();
 
 ///////// FIREBASE CONFIGURATION //////////
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAnnE5fBtpt3WI3_iveGTGgDSWKWijukUM",
-    authDomain: "noted-reef-387021.firebaseapp.com",
-    projectId: "noted-reef-387021",
-    storageBucket: "noted-reef-387021.appspot.com",
-    messagingSenderId: "1012884798546",
-    appId: "1:1012884798546:web:34031d87f26e6feef2f38b",
-    measurementId: "G-5RWVQN75V4"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+///////// MAIN CODE //////////
+// Import Alpine.js and Page.js
 import Alpine from "alpinejs";
 import page from "page";
+
+// Import custom modules
 import setupRoutes from './routes/setUpRoutes';
 import initializeGlobals from "./utils/initializeGlobals";
 import EventListenerManager from './events/EventListenerManager';
