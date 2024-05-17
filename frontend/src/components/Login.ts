32d8740a6aa7902599bbc3298df6fcaa9f7c7365
Michel Dias <michel.sdf@gmail.com>
2024-05-17 16:27:49 -0300

    Log in seems to be working

--- frontend/src/components/Login.ts
+++ frontend/src/components/Login.ts
@@ -5,7 +5,7 @@ function component() {
                 <h1 class="login__title">Log in</h1>
                 <div class="login__form-wrapper" x-data="userLogin()" x-cloak>
                     <form class="login__form" x-on:submit.prevent="loginUser">
-                        <input class="login__input login__input--email" type="email" name="email" id="email" placeholder="Email" required/>
+                        <input class="login__input login__input--email" type="user_name" name="user_name" id="user_name" placeholder="Username" required/>
                         <input class="login__input login__input--password" type="password" name="user_password" id="password" placeholder="Password" required/>
                         <input class="login__input login__input--btn" type="submit" name="submit" value="Log in"/>
                     </form>
