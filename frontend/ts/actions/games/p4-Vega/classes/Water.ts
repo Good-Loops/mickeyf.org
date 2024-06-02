f146e625e3327c004a75ce3b7366de129dd58c3d
Michel Dias <michel.sdf@gmail.com>
2024-06-02 13:20:47 -0300

    Implemented sound when player gets water

--- frontend/ts/actions/games/p4-Vega/classes/Water.ts
+++ frontend/ts/actions/games/p4-Vega/classes/Water.ts
@@ -1,15 +1,21 @@
 import { CANVAS_HEIGHT, CANVAS_WIDTH } from "../../../../utils/constants";
 import { getRandomX, getRandomY } from "../../../../utils/random";
 import checkCollision from "../../../../utils/checkCollision";
+import scales from '../../../../utils/scales';
+
 import Entity from "../../classes/Entity";
+
 import BlackHole from "./BlackHole";
 import P4 from "./P4";
+
 import * as PIXI from 'pixi.js';
+import * as Tone from 'tone';
 
 export default class Water extends Entity {
 
     private startX: number = CANVAS_WIDTH - Entity.gap;
     private startY: number = CANVAS_HEIGHT * .5;
+    private synth: Tone.Synth;
 
     public waterAnim: PIXI.AnimatedSprite;
 
@@ -18,6 +24,7 @@ export default class Water extends Entity {
         stage.addChild(waterAnim);
 
         this.waterAnim = waterAnim;
+        this.synth = new Tone.Synth().toDestination(); // Initialize the synth
 
         waterAnim.x = this.startX - waterAnim.width;
         waterAnim.y = this.startY;
@@ -30,10 +37,19 @@ export default class Water extends Entity {
 
             p4.totalWater += 10;
             new BlackHole(stage, p4.p4Anim);
+
+            this.playSound();
         }
     }
 
     public destroy(): void {
         this.waterAnim.destroy();
+    }
+
+    private playSound(): void {
+        // Play a random note from the pentatonic scale
+        const notes = scales["Pentatonic Scale"].notes;
+        const note = notes[Math.floor(Math.random() * notes.length)];
+        this.synth.triggerAttackRelease(note, '8n');
     }
 }
