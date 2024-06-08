7dcde714f79b6a2df2b11dc691a4b04c81b53ed8
Michel Dias <michel.sdf@gmail.com>
2024-06-08 13:48:57 -0300

    Implemented user interface styles

--- frontend/ts/components/games/P4Vega.ts
+++ frontend/ts/components/games/P4Vega.ts
@@ -19,56 +19,58 @@ function component() {
                         <input class="p4-vega__ui--checkbox" type="checkbox" data-musical-notes-playing>
                         <span class="p4-vega__ui--option-btn">Notes Playing</span>
                     </label>
-                    <div class="p4-vega__ui--dropdown" data-dropdown>
-                        <button class="p4-vega__ui--dropdown-btn" data-dropdown-btn>Key: &nbsp;<span data-selected-key>C</span></button>
-                        <div class="p4-vega__ui--dropdown-menu">
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="C">C</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="C#/Db">C#/Db</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="D">D</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="D#/Eb">D#/Eb</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="E">E</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="F">F</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="F#/Gb">F#/Gb</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="G">G</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="G#/Ab">G#/Ab</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="A">A</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="A#/Bb">A#/Bb</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="B">B</div>
+                    <div class="p4-vega__ui--dropdown-grid">
+                        <div class="p4-vega__ui--dropdown" data-dropdown>
+                            <button class="p4-vega__ui--dropdown-btn" data-dropdown-btn>Key: &nbsp;<span data-selected-key>C</span></button>
+                            <div class="p4-vega__ui--dropdown-menu p4-vega__ui--dropdown-menu-keys">
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="C">C</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="C#/Db">C#/Db</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="D">D</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="D#/Eb">D#/Eb</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="E">E</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="F">F</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="F#/Gb">F#/Gb</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="G">G</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="G#/Ab">G#/Ab</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="A">A</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="A#/Bb">A#/Bb</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="B">B</div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="p4-vega__ui--dropdown" data-dropdown>
-                        <button class="p4-vega__ui--dropdown-btn" data-dropdown-btn>Scale: &nbsp;<span data-selected-scale>Major Scale</span></button>
-                        <div class="p4-vega__ui--dropdown-menu">
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Major Scale">Major Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Minor Scale">Minor Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Pentatonic Scale">Pentatonic Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Blues Scale">Blues Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Dorian Scale">Dorian Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Mixolydian Scale">Mixolydian Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Phrygian Scale">Phrygian Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Lydian Scale">Lydian Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Locrian Scale">Locrian Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Chromatic Scale">Chromatic Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Harmonic Major Scale">Harmonic Major Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Melodic Minor Scale">Melodic Minor Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Whole Tone Scale">Whole Tone Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Hungarian Minor Scale">Hungarian Minor Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Double Harmonic Scale">Double Harmonic Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Neapolitan Major Scale">Neapolitan Major Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Neapolitan Minor Scale">Neapolitan Minor Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Augmented Scale">Augmented Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Hexatonic Scale">Hexatonic Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Enigmatic Scale">Enigmatic Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Spanish Gypsy Scale">Spanish Gypsy Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Hirajoshi Scale">Hirajoshi Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Balinese Pelog Scale">Balinese Pelog Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Egyptian Scale">Egyptian Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Hungarian Gypsy Scale">Hungarian Gypsy Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Persian Scale">Persian Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Tritone Scale">Tritone Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Flamenco Scale">Flamenco Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Iwato Scale">Iwato Scale</div>
-                            <div class="p4-vega__ui--dropdown-menu-item" data-item="Blues Heptatonic Scale">Blues Heptatonic Scale</div>
+                        <div class="p4-vega__ui--dropdown" data-dropdown>
+                            <button class="p4-vega__ui--dropdown-btn" data-dropdown-btn>Scale: &nbsp;<span data-selected-scale>Major</span></button>
+                            <div class="p4-vega__ui--dropdown-menu">
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Major">Major</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Minor">Minor</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Pentatonic">Pentatonic</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Blues">Blues</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Dorian">Dorian</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Mixolydian">Mixolydian</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Phrygian">Phrygian</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Lydian">Lydian</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Locrian">Locrian</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Chromatic">Chromatic</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Harmonic Major">Harmonic Major</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Melodic Minor">Melodic Minor</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Whole Tone">Whole Tone</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Hungarian Minor">Hungarian Minor</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Double Harmonic">Double Harmonic</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Neapolitan Major">Neapolitan Major</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Neapolitan Minor">Neapolitan Minor</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Augmented">Augmented</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Hexatonic">Hexatonic</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Enigmatic">Enigmatic</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Spanish Gypsy">Spanish Gypsy</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Hirajoshi">Hirajoshi</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Balinese Pelog">Balinese Pelog</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Egyptian">Egyptian</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Hungarian Gypsy">Hungarian Gypsy</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Persian">Persian</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Tritone">Tritone</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Flamenco">Flamenco</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Iwato">Iwato</div>
+                                <div class="p4-vega__ui--dropdown-menu-item" data-item="Blues Heptatonic">Blues Heptatonic</div>
+                            </div>
                         </div>
                     </div>
                 </div>
