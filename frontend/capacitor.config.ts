369d5fb9604dc2ffc2332130370e69e3d7e6087c
Michel Dias <michel.sdf@gmail.com>
2024-10-13 13:24:30 -0300

    Prepared for ios and android with capacitor

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.mickeyf.app',
+  appName: 'BeatCalc',
+  webDir: './public'
+};
+
+export default config;
