36ce50be72d2e5d038c1f6980e88fdd8126d7915
Michel Dias <michel.sdf@gmail.com>
2024-10-28 09:39:21 -0300

    Rotating Graphics, lets go

--- frontend/ts/actions/animations/dancing fractals/danceFractals.ts
+++ frontend/ts/actions/animations/dancing fractals/danceFractals.ts
@@ -1,14 +1,19 @@
-import { Application, Graphics } from 'pixi.js';
+import { Application, Graphics, Ticker } from 'pixi.js';
 import { CANVAS_HEIGHT, CANVAS_WIDTH } from '../../../utils/constants';
 
 import FullscreenButton from '../../../helpers/FullscreenButton';
 
 export default async function danceFractals(): Promise<void> {
 
-    const app = new Application();
-    (globalThis as any).__PIXI_APP__ = app; // For debugging with pixi devtools
+    const app: Application = new Application();
+    (globalThis as any).__PIXI_APP__ = app; // For pixi devtools
 
-    await app.init({ antialias: true, backgroundColor: "#334455", width: CANVAS_WIDTH, height: CANVAS_HEIGHT });
+    await app.init({ 
+        antialias: true, 
+        backgroundColor: "#334455",
+        width: CANVAS_WIDTH, 
+        height: CANVAS_HEIGHT 
+    });
 
     const sectionDataAttribute: string = '[data-dancing-fractals]'; 
 
@@ -16,10 +21,23 @@ export default async function danceFractals(): Promise<void> {
 
     new FullscreenButton(app.canvas, sectionDataAttribute);
 
-    const graphics = new Graphics();
+    const line: Graphics = new Graphics();
 
-    graphics.rect(100, 200, 50, 100)
-            .fill(0x6688ff);
+    line.moveTo(200, 200);
+    line.lineTo(300, 300);
+    line.stroke({ color: 0xff0000, width: 30 });
 
-    app.stage.addChild(graphics);
+    app.stage.addChild(line);
+
+    const rectangle: Graphics = new Graphics();
+
+    rectangle.rect(0, 0, 50, 50).fill(0x6688ff);
+    rectangle.pivot.set(25, 25);
+    rectangle.position.set(200, 400);
+
+    app.stage.addChild(rectangle);
+
+    app.ticker.add((time: Ticker) => {
+        rectangle.rotation += 0.01;
+    });
 }
