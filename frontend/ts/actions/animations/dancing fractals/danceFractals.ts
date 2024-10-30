470a528319790e0741c9ade4e507f73a34ef1297
Michel Dias <michel.sdf@gmail.com>
2024-10-30 08:10:45 -0300

    Created first interesting fractal

--- frontend/ts/actions/animations/dancing fractals/danceFractals.ts
+++ frontend/ts/actions/animations/dancing fractals/danceFractals.ts
@@ -1,4 +1,4 @@
-import { Application, Graphics, Ticker } from 'pixi.js';
+import { Application, Graphics, StrokeStyle } from 'pixi.js';
 import { CANVAS_HEIGHT, CANVAS_WIDTH } from '../../../utils/constants';
 
 import FullscreenButton from '../../../helpers/FullscreenButton';
@@ -8,57 +8,168 @@ export default async function danceFractals(): Promise<void> {
     const app: Application = new Application();
     (globalThis as any).__PIXI_APP__ = app; // For pixi devtools
 
-    await app.init({ 
-        antialias: true, 
+    await app.init({
+        antialias: true,
         backgroundColor: "#334455",
-        width: CANVAS_WIDTH, 
-        height: CANVAS_HEIGHT 
+        width: CANVAS_WIDTH,
+        height: CANVAS_HEIGHT
     });
 
-    const sectionDataAttribute: string = '[data-dancing-fractals]'; 
+    const sectionDataAttribute: string = '[data-dancing-fractals]';
 
     document.querySelector(sectionDataAttribute)!.append(app.canvas);
 
     new FullscreenButton(app.canvas, sectionDataAttribute);
 
-    const graphics: Graphics = new Graphics();
+    
+    let line: Graphics = new Graphics();
+    
+    app.stage.addChild(line);
+    
+    const centerY: number = app.screen.height / 2;
 
-    let midX: number = app.screen.width / 2;
-    let midY: number = app.screen.height / 2;
+    // /////// Branch A
+    // // Middle Line A
+    // line.moveTo(0, centerY);
 
-    let lineSize: number = 400;
-    let angle: number = 0;
+    // line.lineTo(200, centerY);
 
-    function drawLine(angle: number) {
-        const endX = midX + lineSize * Math.cos(angle);
-        const endY = midY + lineSize * Math.sin(angle);
+    // // Upper Line A 
+    // line.moveTo(100, centerY);
 
-        graphics.moveTo(midX, midY);
-        graphics.lineTo(endX, endY);
-        graphics.stroke({ color: 0x00aa55, width: 10 });
+    // line.lineTo(100 + 140, centerY - 40);
 
+    // // Lower Line A
+    // line.moveTo(100, centerY);
+    
+    // line.lineTo(100 + 140, centerY + 40);
+
+    // /////// Branch B
+    // // Middle Line B (Half length of root middle line)
+    // line.moveTo(100 + 140, centerY - 40);
+
+    // line.lineTo(100 + 140 + 100, centerY - 40);
+
+    // // Upper Line B
+    // line.moveTo(100 + 140 + 50, centerY - 40);
+
+    // line.lineTo(100 + 140 + 50 + 70, centerY - 40 - 20);
+
+    // // Lower Line B
+    // line.moveTo(100 + 140 + 50, centerY - 40);
+
+    // line.lineTo(100 + 140 + 50 + 70, centerY - 40 + 20);
+
+    // /////// Branch C
+    // // Middle Line C
+    // line.moveTo(100 + 140, centerY + 40);
+
+    // line.lineTo(100 + 140 + 100, centerY + 40);
+
+    // // Upper Line C
+    // line.moveTo(100 + 140 + 50, centerY + 40);
+
+    // line.lineTo(100 + 140 + 50 + 70, centerY + 40 - 20);
+
+    // // Lower Line C
+    // line.moveTo(100 + 140 + 50, centerY + 40);
+
+    // line.lineTo(100 + 140 + 50 + 70, centerY + 40 + 20);
+
+    // line.stroke({ color: 0xffffff, width: 10, cap: 'round' });
+
+    const drawTree = (startX: number, startY: number, levels: number): void => {
+
+        let lineWidth = 10;
+
+        let rootLength: number = 200;
+
+        let branchGrowX: number = 140; 
+        let branchGrowY: number = 40;
+
+        let middleStartX: number = startX;
+        let middleStartY: number = startY;
+
+        let middleEndX: number = startX + rootLength;
+        let middleEndY: number = middleStartY;
+
+        let upperStartX: number = (startX + rootLength) / 2;
+        let upperStartY: number = startY;
+
+        let upperEndX: number = ((startX + rootLength) / 2) + branchGrowX;
+        let upperEndY: number = startY - branchGrowY;
+
+        let lowerStartX: number = (startX + rootLength) / 2;
+        let lowerEndX: number = ((startX + rootLength) / 2) + branchGrowX;
+
+        let lowerStartY: number = startY;
+        let lowerEndY: number = startY + branchGrowY;
+
+        let up: boolean = true;
+
+        for(let i: number = 0; i < levels; i++) {
+            // Middle
+            line.moveTo(middleStartX, middleStartY);
+
+            line.lineTo(middleEndX, middleEndY);
+
+            // Upper
+            line.moveTo(upperStartX, upperStartY);
+
+            line.lineTo(upperEndX, upperEndY);
+
+            // Lower
+            line.moveTo(lowerStartX, lowerStartY)
+
+            line.lineTo(lowerEndX, lowerEndY);
+
+            if(up) {
+                middleStartX = upperEndX;
+                middleStartY = upperEndY;
+
+                middleEndX = upperEndX + (rootLength / 2);
+                middleEndY = middleStartY;
+
+                upperStartX = upperEndX + (rootLength / 4);
+                upperStartY = middleStartY;
+
+                upperEndX = upperStartX + (branchGrowX / 2);
+                upperEndY = upperStartY - (branchGrowY / 2);
+
+                lowerStartX = upperStartX;
+                lowerStartY = upperStartY;
+
+                lowerEndX = upperEndX;
+                lowerEndY = upperStartY + (branchGrowY / 2);
+
+                up = false;
+            } else {
+                middleStartX = lowerEndX;
+                middleStartY = lowerEndY;
+
+                middleEndX = lowerEndX + (rootLength / 2);
+                middleEndY = middleStartY;
+
+                upperStartX = lowerEndX + (rootLength / 4);
+                upperStartY = middleStartY;
+
+                upperEndX = upperStartX + (branchGrowX / 2);
+                upperEndY = upperStartY - (branchGrowY / 2);
+
+                lowerStartX = upperStartX;
+                lowerStartY = upperStartY;
+
+                lowerEndX = upperEndX;
+                lowerEndY = upperStartY + (branchGrowY / 2);
+
+                up = true;
+            }
+
+            line.stroke({ color: 0xffffff, width: lineWidth, cap: 'round' });
+
+            lineWidth -= 1.2;
+        }         
     }
 
-    // for(let angle = 0; angle < Math.PI * 2; angle += 0.1) {  
-    //     drawLine(angle);
-    // }
-
-    app.stage.addChild(graphics);
-
-    // const rectangle: Graphics = new Graphics();
-
-    // rectangle.rect(0, 0, 50, 50).fill(0x6688ff);
-    // rectangle.pivot.set(25, 25);
-    // rectangle.position.set(200, 400);
-
-    // app.stage.addChild(rectangle);
-
-    let seconds: number = 0;
-
-    app.ticker.add((time: Ticker) => {
-        seconds += time.deltaMS / 1000;
-        graphics.clear();
-
-        drawLine(angle + seconds);
-    });
+    drawTree(0, centerY, 10);
 }
