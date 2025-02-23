bbe12d7d6919f50862e97aa6144a6e46062e27b0
Michel Dias <michel.sdf@gmail.com>
2025-02-22 21:05:08 -0300

    Fix Entity class

--- frontend/ts/actions/games/helpers/Entity.ts
+++ frontend/ts/actions/games/helpers/Entity.ts
@@ -1,18 +1,17 @@
 import * as PIXI from "pixi.js";
 
 export default abstract class Entity {
+  abstract update(...args: any[]): void;
 
-    abstract update(): void;
+  static gap = 10;
+  static hitBoxAdjust = 0.8;
 
-    static gap = 10;
-    static hitBoxAdjust = .8;
-    
-    constructor(public anim: PIXI.AnimatedSprite) {
-        this.play(this.anim);
-    }
+  constructor(public anim: PIXI.AnimatedSprite) {
+    this.play(this.anim);
+  }
 
-    play(anim: PIXI.AnimatedSprite): void {
-        anim.animationSpeed = .1;
-        anim.play();
-    }
+  play(anim: PIXI.AnimatedSprite): void {
+    anim.animationSpeed = 0.1;
+    anim.play();
+  }
 }
