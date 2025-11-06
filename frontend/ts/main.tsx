88651524950140bc93e83ffde13c2de12abf724f
Good-Loops <michel.sdf@gmail.com>
2025-11-06 10:00:56 -0300

    Clean up

--- frontend/ts/main.tsx
+++ frontend/ts/main.tsx
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
