e0cb6553efc824b608d25d367ad5003b7d4a692b
Michel Dias <michel.sdf@gmail.com>
2026-01-21 14:03:13 -0300

    Delete utils.ts

--- frontend/ts/public/utils.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Frontend public surface: shared utilities.
- */
-
-export { clamp } from '../utils/clamp';
-export { lerp } from '../utils/lerp';
-export { smoothstep01 } from '../utils/smoothstep01';
